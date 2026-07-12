b78876e08451ac9280d2df9f0d98fea7a3c411ee
Fmeech2 <kto-to.tchyata@yandex.ru>
2026-07-12 18:52:37 +0400

    Первичная загрузка проекта
    + тестовая страница сгенериная ии для проверки вёрстки всех страниц

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+
+  <body>
+    <style>
+      .logo {
+        width: 200px;
+        height: auto;
+      }
+    </style>
+    <!-- <?php echo ' Hello World' ?>
+    <?php echo 'PHP version: ' . phpversion(); ?>-->
+    <img src="img/Logo_zaglyshka.png" alt="Логотип КЛЁВАЯ ДАЧА" class="logo" />
+    <div>
+        Гостевой комплекс
+    </div>
+    <div>
+      Астраханская область, Камызякский район, п. Ильинка на реке Гандурино
+    </div>
+    <div>
+        Аренда дома на Реке в дельте Волги
+    </div>
+  </body>
+</html>
